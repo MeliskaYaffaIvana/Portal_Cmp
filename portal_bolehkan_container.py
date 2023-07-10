d82a6e67fb481e6717a9e4b06f7f7a12b9358e31
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-10 19:25:50 +0700

    portal_bolehkan_container.py
    update bolehkan

--- portal_bolehkan_container.py
+++ portal_bolehkan_container.py
@@ -2,8 +2,9 @@ import time
 import requests
 import mysql.connector
 
+url = 'http://10.0.0.21:8282/api/update_bolehkan_container/'
 # Fungsi untuk membaca nilai dari database berdasarkan ID kontainer
-def read_from_database(id_kontainer):
+def read_from_database():
     # Koneksi ke database
     conn = mysql.connector.connect(
         host="127.0.0.1",
@@ -13,10 +14,10 @@ def read_from_database(id_kontainer):
     )
     cursor = conn.cursor()
 
-    # Eksekusi query untuk membaca nilai dari database
-    query = f"SELECT bolehkan FROM container WHERE id = {id}"
+   # Eksekusi query untuk membaca nilai bolehkan dan id dari tabel kontainer
+    query = "SELECT bolehkan, id FROM container"
     cursor.execute(query)
-    result = cursor.fetchone()
+    results = cursor.fetchall()
 
     # Menutup koneksi database
     cursor.close()
@@ -28,52 +29,22 @@ def read_from_database(id_kontainer):
     else:
         return None
 
-# URL endpoint dan ID kontainer yang ingin diubah
-base_url = 'http://10.0.0.21:8282/api/update_bolehkan_container/'
+# Fungsi untuk mengirim nilai bolehkan dan id ke server Django
+def send_to_server(id, bolehkan):
+    data = {
+        'id': id,
+        'bolehkan': bolehkan
+    }
+    response = requests.post(url, json=data)
 
-
-# Fungsi untuk membaca status container dari server berdasarkan ID kontainer
-def read_status_from_server(id):
-    url = f"{base_url}{id}/get-bolehkan/"
-    response = requests.get(url)  # Kirim permintaan GET untuk membaca status
+    # Memeriksa kode status respons
     if response.status_code == 200:
-        data = response.json()
-        return data['bolehkan']
+        print('Nilai berhasil dikirim ke server')
     else:
-        return None
+        print('Gagal mengirim nilai ke server')
 
-# Fungsi untuk melakukan polling dan mengirim permintaan perubahan
-def poll_and_send():
-    previous_bolehkan = None
-    while True:
-        # Kode untuk membaca nilai dari database berdasarkan ID kontainer
-        bolehkan_database = read_from_database(id)
-
-        # Membaca status container dari server berdasarkan ID kontainer
-        bolehkan_server = read_status_from_server(id)
-
-        # Membandingkan nilai status server dengan nilai status database
-        if bolehkan_server is not None and bolehkan_server != bolehkan_database:
-            # Data yang akan dikirim
-            data = {
-                'bolehkan': bolehkan_database
-            }
-
-            # Kirim permintaan PATCH
-            url = f"{base_url}{id}/update-bolehkan/"
-            response = requests.patch(url, json=data)
-
-            # Periksa kode status respons
-            if response.status_code == 200:
-                print('Permintaan berhasil:', response.json()['message'])
-            else:
-                print('Permintaan gagal:', response.text)
-
-        # Mengupdate nilai sebelumnya
-        previous_bolehkan = bolehkan_database
-
-        # Waktu jeda antara setiap polling (misalnya, 5 detik)
-        time.sleep(5)  # Ganti dengan waktu jeda yang diinginkan
-
-# Panggil fungsi polling
-poll_and_send()
+# Contoh pemanggilan fungsi
+data_kontainer = read_from_database()
+for bolehkan, id in data_kontainer:
+    send_to_server(id, bolehkan)
+     time.sleep(2)  # Jeda 2 detik antara pengiriman setiap kontainer
