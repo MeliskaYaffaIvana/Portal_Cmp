5b6a2fce6a611b0fba537e743cfeadfbcab14769
Myaffna <75622239+MeliskaYaffaIvana@users.noreply.github.com>
2023-07-12 15:46:07 +0700

    Create portal_hapus_kontainer.py

---
+++ portal_hapus_kontainer.py
@@ -0,0 +1,39 @@
+import requests
+import mysql.connector
+
+url = 'http://10.0.0.21:8080/api/delete_kontainer/'
+
+def send_kontainer_names_to_server():
+    # Koneksi ke database
+    conn = mysql.connector.connect(
+        host="127.0.0.1",
+        user="meliska",
+        password="meliska",
+        database="repository"
+    )
+    cursor = conn.cursor()
+
+    # Eksekusi query untuk membaca id dari tabel kontainer
+    query = "SELECT id FROM kontainer"
+    cursor.execute(query)
+    id_kontainer = cursor.fetchall()
+
+    # Menutup koneksi database
+    cursor.close()
+    conn.close()
+
+    # Mengirim id ke server
+    for id in id_kontainer:
+        data = {
+            'id': id
+        }
+        response = requests.post(url, json=data)
+
+        # Memeriksa kode status respons
+        if response.status_code == 200:
+            print(f'Nama template {id} berhasil dikirim ke server')
+        else:
+            print(f'Gagal mengirim nama template {id} ke server')
+
+# Contoh pemanggilan fungsi
+send_template_names_to_server()
